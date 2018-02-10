45a53a648a4b2cbd8a9ee1a2fd2c5ac27fb61664
LXG-Shadow <32156054+LXG-Shadow@users.noreply.github.com>
2018-02-10 18:57:18 +0800

    Updated

--- UDP/client.py
+++ UDP/client.py
@@ -1,39 +1,39 @@
 import socket
 import sys
 
-port = 7777
-mtu = 1500
-udpserver = ("127.0.0.1",6666)
+def newsocket(addr="0.0.0.0",port=6666):
+    try:
+        socketserver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        socketserver.bind((addr,port))
+        print("Sever Open")
+        print("Listen at port %s" % port)
+        return socketserver
+    except:
+        print("Unable to listen at this port %s" % port)
+        return -1
+
+def sendmessges(skt,udpserver=("127.0.0.1",6666)):
+    if skt == -1 :
+        return -1
+    print("Connect to %s at port %s" % udpserver)
+    messagecount = 0
+    while True:
+        try:
+            data = input("Enter the Text:")
+            if data == "stop-client":
+                break
+            skt.sendto(("%s" % data).encode("utf-8"), udpserver)
+            messagecount += 1
+        except:
+            print("Error")
+    return messagecount
 
 try:
-    sktsend = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-except:
-    print("Unable to send at this port %s" % port)
-    sktsend.close()
-    sys.exit()
-
-serverip = input("Enter the sever ip: ")
-serverport = input("Enter the port: ")
-
-try:
+    serverip = input("Enter the sever ip: ")
+    serverport = int(input("Enter the port: "))
     udpserver = (serverip,int(serverport))
 except:
     print("Error")
-    sktsend.close()
     sys.exit()
 
-sktsend.bind(("127.0.0.1",port))
-print("UDP Client open at port %s" % port)
-print("Connect to %s at port %s" % udpserver)
-
-while True:
-    try:
-        data = input("Enter the Text:")
-        if data == "stop-client":
-            break
-        sktsend.sendto(("%s" % data).encode("utf-8"), udpserver)
-    except:
-        print("Error")
-
-print("Client Stop")
-sktsend.close()
+print("Sent %s messages" % sendmessges(newsocket(port=7777),udpserver))
--- UDP/server.py
+++ UDP/server.py
@@ -1,30 +1,33 @@
 import socket
-import sys
 
-port = 6666
-mtu = 1500
-
-try:
-    skt = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-except:
-    print("Unable to listen at this port %s" % port)
-    skt.close()
-    sys.exit()
-
-print("Sever Open")
-skt.bind(("0.0.0.0",port))
-print("Listen at port %s" % port)
-while True:
+def newsocket(addr="0.0.0.0",port=6666):
     try:
-        data, addr = skt.recvfrom(mtu)
-        data = data.decode("utf-8")
-        print("Recieve a message: \"%s\" from %s" % (data, addr))
-        skt.sendto(("-%s-" % data).encode("utf-8"),addr)
+        socketserver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        socketserver.bind((addr,port))
+        print("Sever Open")
+        print("Listen at port %s" % port)
+        return socketserver
     except:
-        print("Error")
-    if data == "stop-server":
-        break
+        print("Unable to listen at this port %s" % port)
+        return -1
 
+def recvmessages(skt,mtu=1024):
+    if skt == -1 :
+        return -1
+    messagecount = 0
+    while True:
+        try:
+            data, addr = skt.recvfrom(mtu)
+            data = data.decode("utf-8")
+            print("Recieve a message: \"%s\" from %s" % (data, addr))
+            skt.sendto(("-%s-" % data).encode("utf-8"), addr)
+            messagecount += 1
+            if data == "stop-server":
+                break
+        except:
+            print("Error")
+    skt.close()
+    print("Server Close")
+    return messagecount
 
-print("Server Close")
-skt.close()
+print("Recieve %s messages" % recvmessages(newsocket()))
